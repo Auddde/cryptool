f6276ed239a986826e2d7198e18047f7afad1998
aude <aude.lalanne4@gmail.com>
2021-09-29 13:18:37 +0200

    #12 Developpement Front : HTML et CSS

---
+++ templates/atomic/footer.php
@@ -0,0 +1,18 @@
+<footer class="footer">
+    <div class="container">
+        <div class="col-start-1 col-end-13 flex">
+            <div class="col-1 col-m-3">
+                Pourquoi utiliser TASKSS ?
+            </div>
+            <div class="col-1 col-m-3">
+                <li>Nos tutos</li>
+                <li>Comment créer une liste ?</li>
+                <li>Comment créer une tâche ?</li>
+                <li>Gérer des listes</li>
+            </div>
+            <div class="col-1 col-m-3">
+                Contactez le support technique
+            </div>
+        </div>
+    </div>
+</footer>
